918b9c82f2bed3be8fedf41d8846ae3a4056d8bf
JinhanM <meijinhan0819@gmail.com>
2020-09-04 18:07:16 +0800

    A new flappy bird game with another tutorial, created with birds, background, pipes and without collision

---
+++ pygame playground.py
@@ -0,0 +1,90 @@
+import pygame
+import sys
+import random
+
+
+WIN_WIDTH = 576
+WIN_HEIGHT = 1024
+
+
+def draw_floor():
+    screen.blit(floor_surface, (floor_x_pos, 900))
+    screen.blit(floor_surface, (floor_x_pos + WIN_WIDTH, 900))
+
+def create_pipe():
+    random_pipe_pos = random.choice(pipe_height)
+    bottom_pipe = pipe_surface.get_rect(midtop=(WIN_WIDTH, random_pipe_pos))
+    top_pipe = pipe_surface.get_rect(midbottom=(WIN_WIDTH, random_pipe_pos - 200))
+    return bottom_pipe, top_pipe
+
+def move_pipes(pipes):
+    for pipe in pipes:
+        pipe.centerx -= 5
+    return pipes
+
+def draw_pipes(pipes):
+    for pipe in pipes:
+        if pipe.bottom >= 1024:
+            screen.blit(pipe_surface, pipe)
+        else:
+            flip_pipe = pygame.transform.flip(pipe_surface, False, True)
+            screen.blit(flip_pipe, pipe)
+
+
+# Game Variable
+gravity = 0.25
+bird_movement = 0
+
+pygame.init()
+screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+clock = pygame.time.Clock()
+
+bg_surface = pygame.image.load('assets/background-night.png').convert()
+bg_surface = pygame.transform.scale2x(bg_surface)
+
+
+floor_surface = pygame.transform.scale2x(pygame.image.load('assets/base.png').convert())
+floor_x_pos = 0
+
+bird_surface = pygame.transform.scale2x(pygame.image.load('assets/bluebird-midflap.png').convert())
+bird_rect = bird_surface.get_rect(center=(100, 512))
+
+pipe_surface = pygame.transform.scale2x(pygame.image.load('assets/pipe-green.png').convert())
+pipe_list = []
+SPAWNPIPE = pygame.USEREVENT
+pygame.time.set_timer(SPAWNPIPE, 1200)
+pipe_height = [400, 600, 800]
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                bird_movement = 0
+                bird_movement -= 8
+        if event.type == SPAWNPIPE:
+            pipe_list.extend(create_pipe())
+
+    # Background
+    screen.blit(bg_surface, (0, 0))
+
+    # Birds
+    bird_movement += gravity
+    bird_rect.centery += bird_movement
+
+    screen.blit(bird_surface, bird_rect)
+
+    # Pipes
+    pipe_list = move_pipes(pipe_list)
+    draw_pipes(pipe_list)
+
+    # Floor
+    floor_x_pos -= 1
+    draw_floor()
+    if floor_x_pos < -576:
+        floor_x_pos = 0
+
+    pygame.display.update()
+    clock.tick(120)
